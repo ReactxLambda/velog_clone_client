ecb2122dd669af5460cb22317e2d5893fedd35f8
qoralswn82 <58283656+qoralswn82@users.noreply.github.com>
2021-08-30 00:53:02 +0900

    라우터 및 메인페이지  추가,  포스트카드 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import ContentsEditing from './Page/ContentsEditing'
+import {Route,Switch,BrowserRouter } from 'react-router-dom';
+import HomePage from './Page/home/HomePage';
+
 function App() {
   const markdownTextArea = true;
   const [textContents, setTextContents] =  useState('');
   const markdownViewer = false;
   return (
     <div className="App">
-      <ContentsEditing />
+      <BrowserRouter >
+      {/* 
+      <Route path="/@:username" component={VelogPage} />
+      <Route path="/setting" component={SettingPage} /> 
+      <Route path="/lists/:type(liked|read)" component={ReadingListPage} />
+      <Route path="/:mode(trending|recent|following)" component={HomePage}/>
+      */}
+      <Switch>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/write" component={ContentsEditing} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
