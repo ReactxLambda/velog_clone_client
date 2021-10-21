8dc6997df584e40a4eaec37bbc5c985f433b9953
baekminju <bmj@jkds.kr>
2021-10-21 18:14:41 +0900

    내벨로그 1. 소개 탭 에디터 연결 완료 2. 글쓰기 태그 미완료 3. 데이터 연결 미완료

--- src/Page/Velog/AboutTab.tsx
+++ src/Page/Velog/AboutTab.tsx
@@ -5,7 +5,7 @@ const AboutTab: React.FC = ({ match }: any) => {
   //I : 소개글 없을 경우
   //E : 소개글 작성할 수 있는 경우
   //U : 소개글 수정할 수 있는 경우 | 소개글이 있는경우
-  let [about, setAbout] = useState('안녕 나는 초기값이야~');
+  let [about, setAbout] = useState('');
   let [type, setType] = useState(about === '' || about === undefined ? 'I' : 'E');
   const handleClickButton = () => {
     if (type === 'E') {
