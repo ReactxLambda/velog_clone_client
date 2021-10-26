851f16c4305af637d614671d9ab245b6de46b0b1
baekminju <bmj@jkds.kr>
2021-10-26 17:24:36 +0900

    임시글 목록 팝업창 연결 완료

--- src/Page/SavesPage.tsx
+++ src/Page/SavesPage.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Header } from './Global/Header/Header';
 import './SavesPage.scss';
 
 const SavesPage: React.FC = () => {
   const [savesContents, setSavesContents] = useState([1, 2, 3, 4, 4, 5, 6, 6]);
+  const [popup, setPopup] = useState(false);
+
   const handleRemoveSaves = (id: any) => {
     //삭제 구현
+    setPopup(true);
   };
   return (
     <div>
@@ -37,19 +40,27 @@ const SavesPage: React.FC = () => {
             })}
         </div>
       </div>
-      <div></div>
-      <div>
-        <div>
-          <div>
-            <h3>임시 글 삭제</h3>
-            <div>임시 저장한 글을 삭제하시겠습니까? 삭제한 글은 복구할 수 없습니다.</div>
-            <div>
-              <button>취소</button>
-              <button>확인</button>
+      {popup && (
+        <Fragment>
+          <div className="what"></div>
+          <div className="savesRemoveWrapper">
+            <div className="savesRemove">
+              <div>
+                <h3>임시 글 삭제</h3>
+                <div className="savesRemoveMessage">
+                  임시 저장한 글을 삭제하시겠습니까? 삭제한 글은 복구할 수 없습니다.
+                </div>
+                <div className="buttonsWrapper">
+                  <button className="savesRemoveCancel" onClick={() => setPopup(false)}>
+                    취소
+                  </button>
+                  <button className="savesRemoveOk">확인</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </Fragment>
+      )}
     </div>
   );
 };
